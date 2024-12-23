a5ecc675bc371a748470adb6504a2acd5f91e623
kheireddinebouiche <kheirddinebouiche@gmail.com>
2024-12-23 11:00:53 +0100

    static files

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from .views import *
 
-app_name="app"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('institut_app.urls',namespace='institut_app')),
     path('formateurs/',include('t_formateurs.urls',namespace='t_formateurs')),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='app:login')
-
+@login_required(login_url='login')
 def Index(request):
     tenant = getattr(request, 'tenant', None)
 
