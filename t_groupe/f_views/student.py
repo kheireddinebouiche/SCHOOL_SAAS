fed86fd1637891461ad50668fa341488c782c61a
kheireddinebouiche <kheirddinebouiche@gmail.com>
2025-10-15 13:30:00 +0200

    updates

--- t_groupe/f_views/student.py
+++ t_groupe/f_views/student.py
@@ -21,7 +21,8 @@ def StudentDetails(request, pk):
     notes = NotesProcpects.objects.filter(prospect = student, context="etudiant")
     rappels = RendezVous.objects.filter(prospect = student, context="etudiant" )
     echeanciers = DuePaiements.objects.filter(client = student, type='frais_f').order_by('ordre')
-    
+
+    remises = RemiseAppliquerLine.objects.filter(prospect = student, remise_appliquer__is_approuved = True,remise_appliquer__is_applicated = False)
 
     montant_due = DuePaiements.objects.filter(client = student, is_done=False, type='frais_f').aggregate(total=Sum('montant_restant'))['total'] or 0
     montant_paye = Paiements.objects.filter(prospect= student, context="frais_f").aggregate(total=Sum('montant_paye'))['total'] or 0
@@ -41,5 +42,6 @@ def StudentDetails(request, pk):
         'montant_due' : montant_due,
         'montant_paye' : montant_paye,
         'total_a_paye' : total_a_paye.specialite.formation.prix_formation + total_a_paye.specialite.formation.frais_inscription,
+        'remises' : remises,
     }
     return render(request, 'tenant_folder/student/profile_etudiant.html',context)
