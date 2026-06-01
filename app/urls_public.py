5a751f30ca44b97c3cdebd808e3314abd193c0a0
kheireddinebouiche <kheirddinebouiche@gmail.com>
2026-06-01 17:24:33 +0100

    updates

--- app/urls_public.py
+++ app/urls_public.py
@@ -4,6 +4,7 @@ from .views import *
 from .admin import tenant_admin_site
 from django.contrib.auth.views import LoginView, LogoutView
 from .api.api import *
+from t_etudiants.api.views import get_student_profile_api, get_student_planning_api, get_student_notes_api, get_student_attendance_api, get_student_finances_api, get_student_notifications_api
 
 
 app_name="app"
@@ -27,6 +28,12 @@ urlpatterns = [
     path('api/v1/institutes/verify/', verify_institute, name='verify_institute'),
     path('api/v1/students/verify/', verify_student, name='verify_student'),
     path('api/v1/students/login/', login_student, name='login_student'),
+    path('api/v1/students/profile/', get_student_profile_api, name='get_student_profile_api'),
+    path('api/v1/students/planning/', get_student_planning_api, name='get_student_planning_api'),
+    path('api/v1/students/notes/', get_student_notes_api, name='get_student_notes_api'),
+    path('api/v1/students/attendance/', get_student_attendance_api, name='get_student_attendance_api'),
+    path('api/v1/students/finances/', get_student_finances_api, name='get_student_finances_api'),
+    path('api/v1/students/notifications/', get_student_notifications_api, name='get_student_notifications_api'),
 
     # path('login/',LoginView.as_view(), name="login"),
     # path('logout/',LogoutView.as_view(), name="logout"),
