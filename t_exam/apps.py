934fbc26b61c63811711f8a68e5ec0bba5185024
kheireddinebouiche <kheirddinebouiche@gmail.com>
2026-06-03 11:51:41 +0100

    updates

--- t_exam/apps.py
+++ t_exam/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TExamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 't_exam'
+
+    def ready(self):
+        import t_exam.signals
